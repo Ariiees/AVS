eb7092e213a294ab6dad501e8d9fb90f209288ea
Ariiees <yuxw@udel.edu>
2024-10-17 19:21:23 -0400

    embedded to BlueICE simulator
    Signed-off-by: Ariiees <yuxw@udel.edu>

--- src/avder/src/edr.cpp
+++ src/avder/src/edr.cpp
@@ -4,6 +4,9 @@
 #include <rosbag2_cpp/writers/sequential_writer.hpp>
 #include <rosbag2_storage/storage_options.hpp>
 #include <std_msgs/msg/bool.hpp>
+#include <v2x_msg/msg/bsm.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <chrono>
 #include <string>
 
@@ -19,6 +22,7 @@ private:
   void handle_message(const std::string &topic_name, std::shared_ptr<rclcpp::SerializedMessage> msg);
   void on_crash_event(const std_msgs::msg::Bool::SharedPtr msg);
   void write_message_to_mcap(const std::string &topic_name, std::shared_ptr<rclcpp::SerializedMessage> msg, rclcpp::Time timestamp);
+  void shutdown_node();
 
   struct BufferedMessage {
     rclcpp::Time timestamp;
@@ -30,17 +34,20 @@ private:
   };
 
   bool is_recording_post_crash_;
+  bool crash_detect_status_;
   boost::circular_buffer<BufferedMessage> pre_crash_data_; // Circular buffer container
   std::shared_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
   std::vector<rclcpp::GenericSubscription::SharedPtr> generic_subscriptions_;
   rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr crash_event_subscriber_;
+  rclcpp::TimerBase::SharedPtr shutdown_timer_; 
 
 };
 
 // SETUP
 VehicleEventRecorder::VehicleEventRecorder() : Node("vehicle_event_data_recorder"),
                                                is_recording_post_crash_(false),
-                                               pre_crash_data_(100) // Buffer size for 100 messages
+                                               crash_detect_status_(true),
+                                               pre_crash_data_(800) // Buffer size for 1600 messages around 20s-30s
 {
   // MCAP setup
   rosbag2_storage::StorageOptions storage_options;
@@ -68,12 +75,16 @@ VehicleEventRecorder::VehicleEventRecorder() : Node("vehicle_event_data_recorder
   for (const auto &topic_and_type : topics_and_types)
   {
     auto topic_name = topic_and_type.first;
-    if (topic_name == "/parameter_events" || topic_name == "/rosout" || topic_name == "/events/read_split") {
-      RCLCPP_INFO(this->get_logger(), "Skipping subscription to: '%s'", topic_name.c_str());
+    if (topic_name.rfind("/AVS/", 0) != 0) {  // rfind returns 0 if "/AVS/" is found at the beginning
+      // RCLCPP_INFO(this->get_logger(), "Skipping non-AVS topic: '%s'", topic_name.c_str());
       continue;
     }
-    RCLCPP_INFO(this->get_logger(), "Detect topic: '%s'", topic_name.c_str());
+    
+    // Get topic info (for dynamic subscription)
+    // RCLCPP_INFO(this->get_logger(), "Detect topic: '%s'", topic_name.c_str());
     auto topic_type = topic_and_type.second[0]; // Assuming one type per topic
+    auto publishers_info = this->get_publishers_info_by_topic(topic_name);
+    auto qos_profile = publishers_info[0].qos_profile();
 
     //create topic before write
     rosbag2_storage::TopicMetadata topic_metadata;
@@ -81,11 +92,11 @@ VehicleEventRecorder::VehicleEventRecorder() : Node("vehicle_event_data_recorder
     topic_metadata.type = topic_type;
     topic_metadata.serialization_format = "cdr";
     writer_->create_topic(topic_metadata);
-    RCLCPP_INFO(this->get_logger(), "create bag write topic: '%s'", topic_name.c_str());
+    RCLCPP_INFO(this->get_logger(), "create bag write for topic: '%s'", topic_name.c_str());
 
     //create subscription
     auto sub = this->create_generic_subscription(
-        topic_name, topic_type, 10, [this, topic_name](std::shared_ptr<rclcpp::SerializedMessage> msg) {
+        topic_name, topic_type, qos_profile, [this, topic_name](std::shared_ptr<rclcpp::SerializedMessage> msg) {
           this->handle_message(topic_name, msg);
         });
     generic_subscriptions_.push_back(sub);
@@ -102,16 +113,23 @@ VehicleEventRecorder::VehicleEventRecorder() : Node("vehicle_event_data_recorder
 void VehicleEventRecorder::handle_message(const std::string &topic_name, std::shared_ptr<rclcpp::SerializedMessage> msg)
 {
   auto timestamp = this->now();
-  pre_crash_data_.push_back(BufferedMessage(timestamp, topic_name, msg)); // Adds message to circular buffer
 
   if (is_recording_post_crash_)
   {
     write_message_to_mcap(topic_name, msg, timestamp);
   }
+  else
+  {
+    pre_crash_data_.push_back(BufferedMessage(timestamp, topic_name, msg)); // Adds message to circular buffer
+  }
 }
 
 void VehicleEventRecorder::on_crash_event(const std_msgs::msg::Bool::SharedPtr msg)
 {
+  if (!crash_detect_status_){
+    return;
+  }
+
   if (msg->data)
   {
     RCLCPP_INFO(this->get_logger(), "Crash detected. Writing pre-crash and post-crash data.");
@@ -125,7 +143,14 @@ void VehicleEventRecorder::on_crash_event(const std_msgs::msg::Bool::SharedPtr m
       write_message_to_mcap(data.topic_name, data.message, data.timestamp);
     }
 
-    RCLCPP_INFO(this->get_logger(), "Buffer writing finish");
+    RCLCPP_INFO(this->get_logger(), "Pre-crash-data writing finish");
+
+    // only record 10s data after crash
+    shutdown_timer_ = this->create_wall_timer(
+    5s, std::bind(&VehicleEventRecorder::shutdown_node, this));
+
+    // only triger once
+    crash_detect_status_ = false;
 
   }
 }
@@ -137,6 +162,11 @@ void VehicleEventRecorder::write_message_to_mcap(const std::string &topic_name, 
     serialized_msg->serialized_data = std::make_shared<rcutils_uint8_array_t>(msg->get_rcl_serialized_message());
     serialized_msg->time_stamp = timestamp.nanoseconds();
     writer_->write(serialized_msg);
+}
+
+void VehicleEventRecorder::shutdown_node() {
+  RCLCPP_INFO(this->get_logger(), "Shutting down node after 10 seconds of recording.");
+  rclcpp::shutdown(); // Shut down the rclcpp context
 }
 
 
